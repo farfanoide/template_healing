b3e6e1d14fbf3b5678767ab6305315cb93c12a11
Ivan Karl <ivan6258@gmail.com>
2014-01-12 00:13:02 -0300

    adds pages model and dynamic page-based navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_filter :fetch_pages
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def fetch_pages
+    @pages = Page.all
+  end
 end
