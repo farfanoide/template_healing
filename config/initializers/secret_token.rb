0e58c3bccbc919a10d644d845239c94f4b898cd1
Ivan Karl <ivan6258@gmail.com>
2014-01-11 21:09:10 -0300

    first commit, absolutely nothing done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TemplateHealing::Application.config.secret_key_base = 'b7318746e230c81a418798d414ca4deaebadd77c36894d9ea94fa0297a23943571ef3111eb14bc905be87c60d6e6fcae02f6df7e1a18a42e3fdf8c1ce74f24b5'
