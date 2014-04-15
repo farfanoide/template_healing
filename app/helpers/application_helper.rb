2a019a9e1b3d47796466866e2914c19fc280e16e
Ivan Karl <ivan6258@gmail.com>
2014-04-15 03:03:24 -0300

    adds icon links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def iconize(icon)
+    "<i class=\"icon-#{icon}\"></i>".html_safe unless icon.nil?
+  end
+
+  def edit_link(object)
+    link_to iconize(:pencil), edit_polymorphic_path(object)
+  end
+
 end
