118320ebf39a05c4bc2ff96d34efe5a33b6b2186
Ivan Karl <ivan6258@gmail.com>
2014-04-22 01:34:17 -0300

    fixes footer nav styles, redirects back after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     end
     if user && user.authenticate(session_params[:password])
       session[:user_id] = user.id
-      redirect_to :root, notice: t('login.success')
+      redirect_to :back, notice: t('login.success')
     else
       redirect_to :back, error: t('login.error')
     end
