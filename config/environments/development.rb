770336431784b0deaf895f73a2b27250cbd73c90
Ivan Karl <ivan6258@gmail.com>
2014-05-15 13:45:22 -0300

    adds icons

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,5 @@ TemplateHealing::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  assets.paths << "#{Rails.root}/app/assets/fonts"
 end
