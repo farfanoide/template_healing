5fa3cf4a760a7f5ef4696b3374971c3a87c72966
Ivan Karl <ivan6258@gmail.com>
2014-07-02 01:27:52 -0300

    removes unnecesary respond_to

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   before_action :set_page, only: [:show, :edit, :update, :destroy]
   before_action :authorize, except: [:show, :index, :home]
 
-  # GET /pages
   def index
     @pages = Page.all
   end
@@ -11,57 +10,45 @@ class PagesController < ApplicationController
     @home = "template healing"
   end
 
-  # GET /pages/1
-  def show
-  end
+  def show; end
 
-  # GET /pages/new
   def new
     @page = Page.new
   end
 
-  # GET /pages/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /pages
   def create
     @page = Page.new(page_params)
-
-    respond_to do |format|
-      if @page.save
-        format.html { redirect_to @page, notice: t(:'pages.create.success')}
-      else
-        format.html { render action: 'new' }
-      end
+    if @page.save
+      redirect_to @page, notice: t(:'pages.create.success')
+    else
+      render action: 'new' 
     end
   end
 
   # PATCH/PUT /pages/1
   def update
-    respond_to do |format|
-      if @page.update(page_params)
-        format.html { redirect_to @page, notice: t(:'pages.update.success')}
-      else
-        format.html { render action: 'edit' }
-      end
+    if @page.update(page_params)
+      redirect_to @page, notice: t(:'pages.update.success')
+    else
+      render action: 'edit' 
     end
   end
 
   # DELETE /pages/1
   def destroy
     @page.destroy
-    respond_to do |format|
-      format.html { redirect_to pages_url }
-    end
+    redirect_to :root
   end
 
   private
-    def set_page
-      @page = Page.find_by!(slug: params[:id])
-    end
+  def set_page
+    @page = Page.find_by!(slug: params[:id])
+  end
 
-    def page_params
-      params.require(:page).permit(:title, :body)
-    end
+  def page_params
+    params.require(:page).permit(:title, :body)
+  end
 end
